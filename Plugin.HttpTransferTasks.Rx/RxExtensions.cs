486df4eef9ea1261fc55177a6d4cdbd99b8ea7cf
Allan Ritchie <allan.ritchie@gmail.com>
2017-12-30 21:17:22 -0500

    acr core

--- Plugin.HttpTransferTasks.Rx/RxExtensions.cs
+++ Plugin.HttpTransferTasks.Rx/RxExtensions.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Reactive;
-using System.ComponentModel;
-using System.Linq.Expressions;
 using System.Reactive.Linq;
-using System.Reflection;
+using Acr;
 
 
-namespace Plugin.HttpTransferTasks.Rx
+namespace Plugin.HttpTransferTasks
 {
     public static class RxExtensions
     {
         public static IObservable<IHttpTask> WhenDataChanged(this IHttpTask task) => task
-            .WhenPropertyChanged(x => x.BytesTransferred)
+            .WhenAnyValue(x => x.BytesTransferred)
             .Select(x => task);
 
 
         public static IObservable<TaskStatus> WhenStatusChanged(this IHttpTask task) =>
-            task.WhenPropertyChanged(x => x.Status);
+            task.WhenAnyValue(x => x.Status);
 
 
         public static IObservable<string> WhenCompleted(this IHttpTask task) => task
@@ -31,30 +28,5 @@ namespace Plugin.HttpTransferTasks.Rx
             tasks.CurrentTasksChanged += handler;
             return () => tasks.CurrentTasksChanged -= handler;
         });
-
-
-        public static IObservable<TRet> WhenPropertyChanged<TSender, TRet>(this TSender This, Expression<Func<TSender, TRet>> expression) where TSender : INotifyPropertyChanged
-        {
-            var p = This.GetPropertyInfo(expression);
-
-            return Observable
-                .FromEventPattern<PropertyChangedEventArgs>(This, nameof(INotifyPropertyChanged.PropertyChanged))
-                .StartWith(new EventPattern<PropertyChangedEventArgs>(This, new PropertyChangedEventArgs(p.Name)))
-                .Where(x => x.EventArgs.PropertyName == p.Name)
-                .Select(x =>
-                {
-                    var value = (TRet)p.GetValue(This);
-                    return value;
-                });
-        }
-
-
-        public static PropertyInfo GetPropertyInfo<TSender, TRet>(this TSender sender, Expression<Func<TSender, TRet>> expression)
-        {
-            var lambda = expression as LambdaExpression;
-            var member = lambda.Body as MemberExpression;
-            var property = sender.GetType().GetRuntimeProperty(member.Member.Name);
-            return property;
-        }
     }
 }
